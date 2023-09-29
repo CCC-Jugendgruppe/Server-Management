145c749f1d4bc626c75238771f5bcdfd636bad29
fingadumbledore <fingadumbledore@gmail.com>
2023-09-29 19:59:43 +0200

    angefangen iso updateter

--- Server/pxe/update_iso.sh
+++ Server/pxe/update_iso.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+new_name = "$RANDOM"
+
 cd ./Laptop-Management/
 
 git reset --hard
 
 git pull
 
-cd iso 
+cd iso
+
+mv preseed.iso $new_name
+
+mv $new_name ../ ../backup/
 
 make clean
 
