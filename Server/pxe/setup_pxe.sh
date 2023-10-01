f1ba067bc60994fb06601a7d496bde45ea479f7e
hyperbel <hyperbel@users.noreply.github.com>
2023-10-01 02:06:41 +0000

    color working?

--- Server/pxe/setup_pxe.sh
+++ Server/pxe/setup_pxe.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo " -=- Variable Setup... -=- \n"
+echo -e " -=- Variable Setup... -=- \n"
 
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
@@ -13,79 +13,79 @@ TFTP_OPTIONS="--secure"
 EOL
 )
 
-echo " ${GREEN} DONE! ${NC} \n"
+echo -e " ${GREEN} DONE! ${NC} \n"
 
-echo " -=- OS Setup... -=- \n"
+echo -e " -=- OS Setup... -=- \n"
 
-echo "Updating OS..."
+echo -e "Updating OS..."
 apt update -y > /dev/null
 
-echo "Upgrading OS..."
+echo -e "Upgrading OS..."
 apt upgrade -y > /dev/null
 
-echo " ${GREEN} DONE! ${NC} \n"
-echo " -=- Configuring PXE Server... -=- \n"
+echo -e " ${GREEN} DONE! ${NC} \n"
+echo -e " -=- Configuring PXE Server... -=- \n"
 
-echo "Stopping Service..."
+echo -e "Stopping Service..."
 systemctl stop isc-dhcp-server
 
-echo "Creating tftpboot directory..."
+echo -e "Creating tftpboot directory..."
 mkdir /tftpboot
 
-echo "Copying EFI modules..."
+echo -e "Copying EFI modules..."
 cp -v /usr/lib/syslinux/modules/efi64/{ldlinux.e64,libutil.c32,menu.c32} /tftpboot
 
-echo "Copying EFI binaries..."
+echo -e "Copying EFI binaries..."
 cp -v /usr/lib/SYSLINUX.EFI/efi64/syslinux.efi /tftpboot
 
-echo "Creating pxelinux.cfg directory..."
+echo -e "Creating pxelinux.cfg directory..."
 mkdir /tftpboot/pxelinux.cfg
 
-echo "Configuring TFTPD..."
+echo -e "Configuring TFTPD..."
 
 echo "$tftpd_content" > /etc/default/tftpd-hpa
 
-echo " ${GREEN} DONE! ${NC} \n"
-echo " -=- Setting up Firewall... -=- \n"
+echo -e " ${GREEN} DONE! ${NC} \n"
+echo -e " -=- Setting up Firewall... -=- \n"
 
-echo "Allowing UDP in firewall..."
+echo -e "Allowing UDP in firewall..."
 ufw allow 69/udp
 
-echo "Allowing SSH in firewall..."
+echo -e "Allowing SSH in firewall..."
 ufw allow 22/tcp
 
-echo "Enabling firewall..."
+echo -e "Enabling firewall..."
 ufw enable
 
-echo "restarting tftpd-hpa..."
+echo -e "restarting tftpd-hpa..."
 systemctl restart tftpd-hpa
 
-echo " ${GREEN} DONE! ${NC} \n"
-echo " -=- Configuring DHCP Server... -=- \n"
+echo -e " ${GREEN} DONE! ${NC} \n"
+echo -e " -=- Configuring DHCP Server... -=- \n"
 
-echo "Overwriting dhcpcd configuration..."
+echo -e "Overwriting dhcpcd configuration..."
 cp -v ./dhcpcd.conf /etc/dhcp/dhcpcd.conf
 
-echo "Overwriting isc-dhcp-server configuration..."
+echo -e "Overwriting isc-dhcp-server configuration..."
 cp -v ./isc-dhcp-server /etc/default/isc-dhcp-server
 
-echo "Restarting isc-dhcp-server..."
+echo -e "Restarting isc-dhcp-server..."
 systemctl restart isc-dhcp-server
 
-echo " ${GREEN} DONE! ${NC} \n"
-echo " -=- Setting up PXE... -=- \n"
+echo -e " ${GREEN} DONE! ${NC} \n"
+echo -e " -=- Setting up PXE... -=- \n"
 
-echo "Mounting ISO..."
+echo -e "Mounting ISO..."
 mount -o loop Laptop-Management/preseed.iso /mnt
 mkdir /tftpboot/debian
 
-echo "Copying ISO contents to tftpboot..."
+echo -e "Copying ISO contents to tftpboot..."
 cp  -v -r /mnt/* /tftpboot/debian
 
-echo "Unmounting ISO..."
+echo -e "Unmounting ISO..."
 umount /mnt
 
-echo "Copying PXE configuration..."
+echo -e "Copying PXE configuration..."
 cp -v ./default /tftpboot/pxelinux.cfg/
 
-echo " ${GREEN} DONE! ${NC} \n"
+echo -e " ${GREEN} DONE! ${NC} \n"
